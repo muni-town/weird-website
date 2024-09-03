8ffdc6408e6430833458f10eb7568036afd82c3f
hnb-ku <xyz.hunabku@gmail.com>
2024-09-03 08:39:54 +0400

    DEV: adds a reusable Content component for subpages

---
+++ src/routes/(app)/components/Content.svelte
@@ -0,0 +1,92 @@
+<script>
+  import Markdown from "$lib/Markdown.svelte";
+
+  export let heading;
+  export let description;
+  export let rawMarkdown;
+</script>
+
+<div class="container">
+  <div class="title">
+    <h1>{heading}</h1>
+    <p>{description}</p>
+  </div>
+  <div class="content">
+    <Markdown {rawMarkdown} />
+  </div>
+</div>
+
+<style>
+  .container {
+    color: white;
+    padding-bottom: 15em;
+
+    .title {
+      display: grid;
+      grid-template-columns: 0.4fr auto;
+      padding: 2em;
+      place-items: center;
+
+      gap: 2em;
+
+      h1 {
+        font-family: "Rubik Mono One", monospace;
+        font-size: 3.25em;
+        margin: 0;
+        width: 400px;
+        line-height: 1.5;
+        letter-spacing: 0.5px;
+        user-select: none;
+      }
+
+      p {
+        font-size: 2em;
+        margin: 0;
+        line-height: 1.5;
+      }
+
+      @media (max-width: 1200px) {
+        grid-template-columns: 1fr;
+        h1 {
+          font-size: 2.5em;
+          width: 100%;
+        }
+
+        p {
+          font-size: 1.5em;
+        }
+      }
+    }
+
+    .content {
+      margin: 8em 0 12em 0;
+      background: #683481;
+      padding: 5em 8em;
+      border-radius: var(--border-radius);
+      border: 2px solid black;
+
+      /* these are styles for cooked markdown
+         without :global they get purged
+      */
+      :global(p) {
+        margin-bottom: 1.5em;
+        font-size: 2em;
+      }
+
+      :global(a) {
+        color: white;
+      }
+
+      :global(ul) {
+        font-size: 2em;
+        margin-bottom: 1.5em;
+        padding-left: 1em;
+        list-style-type: disc;
+
+        :global(li) {
+          margin-bottom: 0.5em;
+        }
+      }
+    }
+  }
+</style>
