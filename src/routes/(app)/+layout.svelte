9c51121113c3d806bac2debc414d00ccd9cd520c
hnb-ku <xyz.hunabku@gmail.com>
2024-09-03 08:39:15 +0400

    DEV: adds a shared layout for subpages

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+  import SiteHeader from "$lib/SiteHeader.svelte";
+</script>
+
+<SiteHeader />
+<div class="stars"></div>
+<main>
+  <slot />
+</main>
+
+<style>
+  :global(body) {
+    background: linear-gradient(180deg, #250941 25%, #de7287);
+    min-height: 100vh;
+  }
+
+  .stars {
+    background: url("/stars.avif");
+    height: 40vh;
+    width: 100%;
+    background-position: center;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: -1;
+  }
+</style>
