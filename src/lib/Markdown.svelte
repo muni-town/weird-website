f7eefbf96762287e2a6168ddd8e71e21d2bdf7ca
hnb-ku <xyz.hunabku@gmail.com>
2024-09-04 18:11:38 +0400

    FEATURE: creates a new reusable "Markdown" compenent

---
+++ src/lib/Markdown.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { marked } from "marked";
+  export let rawMarkdown;
+</script>
+
+<div class="markdown-content">
+  {@html marked(rawMarkdown)}
+</div>
+
+<style>
+  .markdown-content {
+    /* these are styles for cooked markdown
+       without :global they get purged
+    */
+
+    :global(p) {
+      margin-bottom: 1.5em;
+      font-size: 2em;
+    }
+
+    :global(a) {
+      color: currentColor;
+    }
+
+    :global(ul) {
+      font-size: 2em;
+      margin-bottom: 1.5em;
+      padding-left: 1em;
+      list-style-type: disc;
+
+      :global(li) {
+        margin-bottom: 0.5em;
+      }
+    }
+  }
+</style>
