2c075f38310342053f4ccd94053818e90e7919b4
Joe <33972521+hnb-ku@users.noreply.github.com>
2024-09-05 07:16:05 +0400

    DEV: updates "feature" cards on the landing page (#10)
    This PR does two things:
    
    1. Adds a new reusable `<Markdown>` component that takes raw markdown and cooks it
    2. Updates the "features" section on the landing page with new copy and makes it use the new `<Markdown>` component.

---
+++ src/routes/components/feature-cards/magic-website-creator.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Card from "./card.svelte";
+  import rawMarkdown from "./magic-website-creator.md?raw";
+  import Markdown from "$lib/Markdown.svelte";
+</script>
+
+<Card>
+  <span slot="icon">➢</span>
+  <span slot="title">Magic Website Creator</span>
+
+  <Markdown {rawMarkdown} slot="body" />
+</Card>
