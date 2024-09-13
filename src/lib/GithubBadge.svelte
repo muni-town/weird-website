1eb82b56c5e03a34f87bdbc16a19fa217dbde7f1
Joe <33972521+hnb-ku@users.noreply.github.com>
2024-09-13 12:00:12 +0400

    FIX: update Github badge repository  (#17)
    1. Right now, we're linking to the website repository. We want it to actually link to Weird's repository.
    2. Adds a preload link for the script needed for the badge. 
    
    (We should probably just skip the badge "widget" altogether and create the link with a build-time `fetch` to get the stars + icon.)

--- src/lib/GithubBadge.svelte
+++ src/lib/GithubBadge.svelte
@@ -1,10 +1,10 @@
 <div class="badge">
   <a
     class="github-button"
-    href="https://github.com/muni-town/weird-website"
+    href="https://github.com/muni-town/weird"
     data-color-scheme="no-preference: light; light: light; dark: light;"
     data-show-count="true"
-    aria-label="Star muni-town/weird-website on GitHub">Star</a
+    aria-label="Star muni-town/weird on GitHub">Star</a
   >
   <script async defer src="https://buttons.github.io/buttons.js"></script>
 </div>
