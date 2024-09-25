660f4c3b3dd7102486e35c9d24c2d53b9f56870f
hnb-ku <xyz.hunabku@gmail.com>
2024-09-25 11:46:40 +0400

    DEV: use the new YoutubeEmbed component

--- src/routes/components/further-reading/Video.svelte
+++ src/routes/components/further-reading/Video.svelte
@@ -1,34 +1,21 @@
 <script>
   import Markdown from "$lib/Markdown.svelte";
+  import YouTubeEmbed from "$lib/YouTubeEmbed.svelte";
+
   import rawMarkdown from "./video.md?raw";
 
-  const videoTitle = "Agentic Product Development - Erlend Sogge Heggen";
-  const videoSrc = "https://www.youtube-nocookie.com/embed/YgYKKCmcE04";
+  const videoId = "YgYKKCmcE04";
+  const title = "Agentic Product Development - Erlend Sogge Heggen";
 </script>
 
-<div class="container video-container">
+<div class="container">
   <Markdown {rawMarkdown} />
-
-  <iframe src={videoSrc} title={videoTitle} loading="lazy" allowfullscreen
-  ></iframe>
+  <YouTubeEmbed {videoId} {title} />
 </div>
 
 <style>
-  .video-container {
-    --apect-ratio: 16 / 9;
-
+  .container {
     padding-top: 12em;
     padding-bottom: 6em;
-
-    iframe {
-      width: 1200px;
-      max-width: 100%;
-      max-height: 85vh;
-      aspect-ratio: var(--apect-ratio);
-      border-radius: var(--border-radius);
-      box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.5);
-      margin-top: 5em;
-      border: 0;
-    }
   }
 </style>
